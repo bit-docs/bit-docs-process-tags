d1db1b981ea6f528ba5966f5f4e83322cb3c72af
Justin Meyer <justinbmeyer@gmail.com>
2016-06-01 17:24:42 -0500

    adding bit-docs release scripts

--- bit-docs.js
+++ bit-docs.js
@@ -1,8 +1,11 @@
 var postProcessTags = require("./post-process");
 var processTags = require("./process-tags");
-
+var _ = require("lodash");
 module.exports = function(bitDocs){
     // this makes tags run
     bitDocs.register("postProcessor", postProcessTags);
     bitDocs.register("processor", processTags);
+    bitDocs.handle("tags", function(siteConfig, tags){
+        siteConfig.tags = _.assign(siteConfig.tags || {}, tags);
+    });
 };

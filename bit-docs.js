7fb25876bfe661ed61fa30d4920bd1f5305cc19c
Joel Kuzmarski <leoj3n@gmail.com>
2017-07-13 19:36:32 -0500

    Update documentation for bit-docs.js

--- bit-docs.js
+++ bit-docs.js
@@ -1,3 +1,7 @@
+var postProcessTags = require("./post-process");
+var processTags = require("./process-tags");
+var _ = require("lodash");
+
 /**
  * @parent plugins
  * @module {function} bit-docs-process-tags
@@ -8,11 +12,24 @@
  *
  * @body
  *
- * TBD
+ * This plugin registers onto these hooks:
+ *   - `processor`
+ *   - `postProcessor`
+ * 
+ * This plugin handles the `tags` hook, and in doing so also adds some tags:
+ *   - [bit-docs-process-tags/tags/add @add]
+ *   - [bit-docs-process-tags/tags/body @body]
+ *   - [bit-docs-process-tags/tags/description @description]
+ *   - [bit-docs-process-tags/tags/hide @hide]
+ *   - [bit-docs-process-tags/tags/parent @parent]
+ * 
+ * Registering the `processor` hook adds a general processor that processes a
+ * code comments and produces a [bit-docs/types/docObject] from the tags found.
+ *
+ * Registering the `postProcessor` hook adds a special processor that calls the
+ * `done` method of any tags that have such a method, passing the
+ * [bit-docs/types/docObject].
  */
-var postProcessTags = require("./post-process");
-var processTags = require("./process-tags");
-var _ = require("lodash");
 module.exports = function(bitDocs){
     // this makes tags run
     bitDocs.register("postProcessor", postProcessTags);
